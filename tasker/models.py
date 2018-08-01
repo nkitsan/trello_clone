c755e8fc512d65673b625c23379f33e98eea8f09
nkitsan <nastik.kitsan@gmail.com>
2018-08-01 23:20:54 +0300

    Added: Private and Public Lists, Calendar and HabitTracker Models

--- tasker/models.py
+++ tasker/models.py
@@ -5,8 +5,38 @@ class Comment(models.Model):
     comment = models.TextField()
 
 
-class Date(models.Model):
-    repeat_date = models.DateTimeField()
+class Remember(models.Model):
+    repeat_date = models.TimeField()
+
+
+class WeeklyRepeat(models.Model):
+    repeat = models.SmallIntegerField()
+
+
+class Habit(models.Model):
+    STATUS = (
+        ('NS','NOT STARTED'),
+        ('F', 'FINISHED')
+    )
+    name = models.CharField(max_length=200, blank=False)
+    status = models.CharField(max_length=200, choices=STATUS)
+    timeline = models.IntegerField()
+
+
+class HabitTracker(models.Model):
+    habits = models.Field.one_to_many(Habit)
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    event_date = models.DateTimeField()
+    remember = models.Field.one_to_many(Remember)
+    repeat = models.OneToOneField(Remember)
+    comments = models.Field.one_to_many(Comment)
+
+
+class Calender(models.Model):
+    events = models.Field.one_to_many(Event)
 
 
 class Subtask(models.Model):
@@ -31,24 +61,31 @@ class Task(models.Model):
     deadline = models.DateTimeField()
 
 
-class Habbit(models.Model):
-    STATUS = (
-        ('NS','NOT STARTED'),
-        ('F', 'FINISHED')
-    )
-    name = models.CharField(max_length=200, blank=False)
-    status = models.CharField(max_length=200, choices=STATUS)
-    timeline = models.IntegerField()
+class PublicTask(models.Model):
+    task = models.OneToOneField(Task)
+    #executors = models.Field.one_to_many(User)
 
 
-class Event(models.Model):
+class PrivateTask(models.Model):
+    task = models.OneToOneField(Task)
+    repeat = models.Field.one_to_many(WeeklyRepeat)
+    remember = models.Field.one_to_many(Remember)
+
+
+class WeeklyList(models.Model):
     name = models.CharField(max_length=200, blank=False)
-    event_date = models.DateTimeField()
-    repeat = models.Field.one_to_many(Date)
-    comments = models.Field.one_to_many(Comment)
+    tasks = models.Field.one_to_many(PrivateTask)
+
+
+class List(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    tasks = models.Field.one_to_many(PrivateTask)
 
 
 class User(models.Model):
     email = models.EmailField(unique='True', blank=False)
     username = models.CharField(max_length=30, unique='True', blank=False)
     password_hash = models.CharField(max_length=200, blank=False)
+    week_list = models.OneToOneField(WeeklyList)
+    habit_tracker = models.OneToOneField(HabitTracker)
+    calendar = models.OneToOneField(Calender)
