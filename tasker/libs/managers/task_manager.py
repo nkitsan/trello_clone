5d7ee24d49fdd80a8f60bdbceffa9a17693047fb
nkitsan <nastik.kitsan@gmail.com>
2018-08-16 20:30:32 +0300

    Added: manager for tasks

---
+++ tasker/libs/managers/task_manager.py
@@ -0,0 +1,57 @@
+"""
+This manager control server-side work with a data and responsible for changing tasks in userlists
+"""
+from tasker.models import Task, Subtask
+
+
+def create_task(name):
+    pass
+
+
+def edit_task_name():
+    pass
+
+
+def add_comment():
+    pass
+
+
+def change_comment():
+    pass
+
+
+def delete_comment():
+    pass
+
+
+def change_status():
+    pass
+
+
+def add_subtask():
+    pass
+
+
+def change_subtask():
+    pass
+
+
+def delete_subtask():
+    pass
+
+
+def change_subtask_status():
+    pass
+
+
+def add_deadline():
+    pass
+
+
+def change_deadline():
+    pass
+
+
+def delete_deadline():
+    pass
+
