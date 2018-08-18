38b54f839125fa016183ece7dd9d52d902640d2d
nkitsan <nastik.kitsan@gmail.com>
2018-08-18 11:40:50 +0300

    Fixed: deleting and changing of events through id

--- tasker/libs/managers/calendar_manager.py
+++ tasker/libs/managers/calendar_manager.py
@@ -4,7 +4,6 @@ Manager for adding, deleting and changing events in the calendar
 
 
 from tasker.models import User, Calender, Event
-from .task_manager import *
 
 
 def create_event(username, event_name, event_date):
@@ -12,47 +11,47 @@ def create_event(username, event_name, event_date):
     user.calendar.events.create(name=event_name, event_date=event_date)
 
 
-def delete_event(username, event_name):
+def delete_event(username, event_id):
     user = User.objects.get(username=username)
-    user.calendar.events.delete(name=event_name)
+    user.calendar.events.delete(id=event_id)
 
 
-def change_event_name(username, event_name, new_event_name):
+def change_event_name(username, event_id, new_event_name):
     user = User.objects.get(username=username)
-    event = user.calendar.events.get(event_name=event_name)
+    event = user.calendar.events.get(id=event_id)
     event.name = new_event_name
     event.save()
 
 
-def change_event_date(username, event_name, new_event_date):
+def change_event_date(username, event_id, new_event_date):
     user = User.objects.get(username=username)
-    event = user.calendar.events.get(event_name=event_name)
+    event = user.calendar.events.get(id=event_id)
     event.event_date = new_event_date
     event.save()
 
 
-def add_event_comment(username, event_name, comment):
+def add_event_comment(username, event_id, comment):
     user = User.objects.get(username=username)
-    event = user.calendar.events.get(event_name=event_name)
+    event = user.calendar.events.get(id=event_id)
     event.comments.create(comment=comment)
 
 
-def delete_event_comment(username, event_name, comment):
+def delete_event_comment(username, event_id, comment_id):
     user = User.objects.get(username=username)
-    event = user.calendar.events.get(event_name=event_name)
-    event.comments.delete(comment=comment)
+    event = user.calendar.events.get(id=event_id)
+    event.comments.delete(id=comment_id)
 
 
-def add_event_remember(username, event_name, remember):
+def add_event_remember(username, event_id, remember):
     user = User.objects.get(username=username)
-    event = user.calendar.events.get(event_name=event_name)
+    event = user.calendar.events.get(id=event_id)
     event.remember.create(repeate_date=remember)
 
 
-def delete_event_remember(username, event_name, remember):
+def delete_event_remember(username, event_id, remember_id):
     user = User.objects.get(username=username)
-    event = user.calendar.events.get(event_name=event_name)
-    event.remember.delete(repeate_date=remember)
+    event = user.calendar.events.get(id=event_id)
+    event.remember.delete(id=remember_id)
 
 
 
